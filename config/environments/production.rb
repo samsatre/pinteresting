980438c92bcb3d65cbf638956037ed0d1132d306
samsatre <slsatre@gmail.com>
2014-03-03 21:19:02 -0500

    styled image page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,13 @@ Pinteresting::Application.configure do
   
     # not to set this to your actual host
   config.action_mailer.default_url_options = { :host => 'http://samsfirst.herokuapp.com/' }
+    
+    config.paperclip_defaults = {
+        :storage => :s3,
+        :s3_credentials => {
+            :bucket => ENV['AWS_BUCKET'],
+            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        }
+}
 end
